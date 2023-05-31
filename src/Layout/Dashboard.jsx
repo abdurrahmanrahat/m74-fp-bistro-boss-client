2078f96db6143e5f6ca2a0132cb3cdf66ea498a7
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-31 18:28:16 +0600

    Dashboard working done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,13 +1,15 @@
 import { FaBookMedical, FaCalendarAlt, FaHome, FaShoppingBag, FaShoppingCart, FaStream, FaUsers, FaUtensils, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
 
     // TODO: load data from db for dynamic admin role
-    const isAdmin = true;
+    // const isAdmin = true;
+    const [isAdmin] = useAdmin();
 
     return (
         <div className="drawer drawer-mobile">
